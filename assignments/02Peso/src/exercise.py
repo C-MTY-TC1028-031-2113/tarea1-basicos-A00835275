80f371b860a5b79781bbaa95d593e0542b801ea1
Axel <A00835275@tec.mx>
2021-08-19 15:59:22 -0500

    Ejercicio 2

--- assignments/02Peso/src/exercise.py
+++ assignments/02Peso/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    PI= float(input("Dame el peso inicial: "))
+    PF= float(input("Dame el peso final: "))
+    CM= float(input("Dame la cantidad de meses: "))
+    prom = float(PI - PF)/ CM 
+    print("Lo que debes bajar por mes es: " + str(prom)) 
 
 
 
