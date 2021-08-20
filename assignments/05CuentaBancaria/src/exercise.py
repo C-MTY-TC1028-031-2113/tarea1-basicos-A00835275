ca839c61a736f9c206b1c32672e1d1ddc07873eb
Axel <A00835275@tec.mx>
2021-08-19 20:36:36 -0500

    Ejercicio 5

--- assignments/05CuentaBancaria/src/exercise.py
+++ assignments/05CuentaBancaria/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    SMA= float(input("Dame el saldo del mes anterior: "))
+    IN=  float(input("Dame los ingresos: "))
+    EG=  float(input("Dame los egresos: "))
+    NC=  float(input("Dame el número de cheques: "))
+    prom = float((SMA + IN - EG ) - (NC * 13))
+    prom = prom * .925
+    print("El saldo final de la cuenta es: " + str(prom))
 
 
 
